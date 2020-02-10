936409052f589b5e7785cc2b50190505a8be2929
zengchen <zengchen98@163.com>
2020-02-10 16:29:09 +0800

    commit

---
+++ 710.黑名单中的随机数.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=710 lang=javascript
+ *
+ * [710] 黑名单中的随机数
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @param {number[]} blacklist
+ */
+var Solution = function(N, blacklist) {
+    
+};
+
+/**
+ * @return {number}
+ */
+Solution.prototype.pick = function() {
+    
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(N, blacklist)
+ * var param_1 = obj.pick()
+ */
+// @lc code=end
+
